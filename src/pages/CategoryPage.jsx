4bb7442cbf14894bb8176608c9dcd50839abc410
AdelieC <castel.adelie@yahoo.com>
2022-04-09 03:54:05 +0200

    category page translation done

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -10,8 +10,10 @@ import BookThumbnail from '../components/reused/BookThumbnail';
 import ActionButton from '../components/reused/ActionButton';
 import { BiAddToQueue } from 'react-icons/bi';
 import { CLASSLIST_H1 } from '../services/globals/classlists';
+import { useTranslation } from 'react-i18next';
 
 function CategoryPage() {
+    const { t } = useTranslation('category');
     const { setTitle } = useOutletContext();
     const { category } = useParams();
     const { books, noResult, setCategory, getNextPage, hasNextPage, reset } =
@@ -25,7 +27,7 @@ function CategoryPage() {
 
     return (
         <div className="flex flex-col items-center justify-center p-8 gap-8">
-            <h1 className={CLASSLIST_H1 + 'text-secondaryDark'}>Books in {category}</h1>
+            <h1 className={CLASSLIST_H1 + 'text-secondaryDark'}>{t('h1') + category}</h1>
             <ul className="flex flex-col gap-8 items-center justify-center list-none">
                 {books?.map((book, i) => {
                     return (
@@ -40,11 +42,11 @@ function CategoryPage() {
                     icon={<BiAddToQueue className="w-4 h-4" />}
                     action={getNextPage}
                     background={'bg-secondaryDark'}
-                    text={'Load more results'}
+                    text={t('load-more-button')}
                     textColor={'text-secondaryLight'}
                 />
             )}
-            {noResult && <p>No book corresponding to this category...</p>}
+            {noResult && <p>{t('no-result')}</p>}
         </div>
     );
 }
