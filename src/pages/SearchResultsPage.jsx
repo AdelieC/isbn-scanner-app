994c31e59935d0a82f0a79652dc9b2ee709adf73
AdelieC <castel.adelie@yahoo.com>
2022-04-09 04:35:00 +0200

    search results en trans done

--- src/pages/SearchResultsPage.jsx
+++ src/pages/SearchResultsPage.jsx
@@ -1,22 +1,33 @@
-import { useSearchParams } from 'react-router-dom';
+//libraries
+import { useOutletContext, useSearchParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+
+//services
 import { CLASSLIST_H1 } from '../services/globals/classlists';
+
+//components
 import WorkInProgress from '../components/reused/WorkInProgress';
 
-SearchResultsPage.propTypes = {};
-
-function SearchResultsPage(props) {
+function SearchResultsPage() {
+    const { t } = useTranslation('search-results');
+    const { setTitle } = useOutletContext();
     const [searchParams] = useSearchParams();
     const author = searchParams.get('author');
     const isbn = searchParams.get('isbn');
     const title = searchParams.get('title');
 
+    useEffect(() => {
+        setTitle(t('page-title'));
+    }, []);
+
     return (
         <div className="grow flex flex-col gap-4 p-4 sm:p-8">
-            <h1 className={CLASSLIST_H1 + 'text-secondaryDark'}>Results for</h1>
+            <h1 className={CLASSLIST_H1 + 'text-secondaryDark'}>{t('h1')}</h1>
             <ul className="font-heading text-base text-primaryDark list-disc">
-                <li>Author = {author || 'empty'}</li>
-                <li>ISBN = {isbn || 'empty'}</li>
-                <li>Title = {title || 'empty'}</li>
+                <li>Author = {author || t('empty-field')}</li>
+                <li>ISBN = {isbn || t('empty-field')}</li>
+                <li>Title = {title || t('empty-field')}</li>
             </ul>
             <WorkInProgress />
         </div>
