6d6d5d3569be31e70c57f95c17fa54dad3b03db4
AdelieC <castel.adelie@yahoo.com>
2022-04-11 22:52:11 +0200

    fixed bug with cameralist index

--- src/services/hooks/useScan.jsx
+++ src/services/hooks/useScan.jsx
@@ -196,7 +196,8 @@ function useScan() {
     const changeCamera = () => {
         const currentIndex = cameraList.indexOf(cameraId);
         let newIndex = 0;
-        if (currentIndex !== cameraList.length - 1) newIndex = currentIndex + 1;
+        if (currentIndex !== cameraList.length - 2) newIndex = currentIndex + 1;
+        else newIndex = 0;
         const newId = cameraList[newIndex];
         stopScan();
         setCameraId(newId);
